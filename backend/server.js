1359ec9c9bf5244116be454387bc6eddfa9fe871
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-02-02 17:48:24 +0300

    create the API endpoint routes

--- backend/server.js
+++ backend/server.js
@@ -22,4 +22,59 @@ const withDB = async operations => {
     }
 }
 
+app.get('/api/articles/:name', async (req, res) => {
+    const articleName = req.params.name;
+    await withDB(async db => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+    });
+});
+
+app.get('/api/articles', async (req, res) => {
+
+    await withDB(async db => {
+        const articlesInfo = await db.collection('articles').find().toArray();
+        res.status(200).json(articlesInfo);
+    });
+});
+
+app.post('/api/articles/:name', async (req, res) => {
+    const articleName = req.params.name;
+
+    await withDB(async db => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+    });
+});
+app.post('/api/articles/:title/upvote', async (req, res) => {
+    const articleName = req.params.title;
+
+    await withDB(async db => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, { '$set': {
+            upvotes: articleInfo.upvotes + 1,
+        }});
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleInfo);
+    });
+});
+app.post('/api/articles/:name/add-comment', async (req, res) => {
+    const articleName = req.params.name;
+    const newComment = req.body;
+
+    await withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        articleInfo.comments.push(newComment);
+
+        await db.collection('articles').updateOne({ name: articleName }, { '$set': {
+            comments: articleInfo.comments
+        }});
+
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleInfo);
+    });
+});
+
+
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
