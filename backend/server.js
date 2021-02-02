9f70c4932af4a2a8a3efd5e24fb587f2a6eaba05
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-02-02 17:47:03 +0300

    connect to database in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,17 @@ app.use(express.json());
 
 const port = process.env.PORT || 8000;
 
+const withDB = async operations => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db('react-blog');
+
+        await operations(db);
+
+        client.close();
+    } catch (err) {
+        res.status(500).send({ message: 'Database Error', err });
+    }
+}
+
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
