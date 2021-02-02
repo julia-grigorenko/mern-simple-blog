b26e68f7615910d6674a3f68f3be2b6937c4cc07
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-02-02 19:26:31 +0300

    screate navbar component and set up react routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {  
+  BrowserRouter as Router,
+  Route, 
+  Switch,
+} from 'react-router-dom';
+
+import NavBar from './NavBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+        <NavBar />
+          <div id="page-body">
+            <Switch>
+              <Route path="/"         component={HomePage}  exact />
+              <Route path="/articles/:name"  component={ArticlePage}  />
+              <Route path="/articles"     component={ArticlesListPage}  exact/>
+              <Route path="/about"    component={AboutPage} exact />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+      
+    );
+  }
 }
 
 export default App;
